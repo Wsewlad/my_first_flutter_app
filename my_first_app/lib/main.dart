bf71a665ca82a626476c66b0a10c394054976834
vfil <wladyslawfil@gmail.com>
2023-03-04 22:48:58 +0200

    Add FavoritesPage

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -177,5 +177,37 @@ class BigCard extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text('You have ${appState.favorites.length} favorites:'),
+          ),
+
+          for (var favorite in appState.favorites)
+            ListTile(
+              title: Text(favorite.asPascalCase),
+              leading: Icon(Icons.favorite),
+              onTap: () {
+                print("remove from favorites");
+                appState.favorites.remove(favorite);
+              },
+            ),
+        ],
+      );
   }
 }
